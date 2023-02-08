766e5c5b042de143c5fed4b5fdf82ee1f5d66c48
Mustafa UYAR <Mustafauyar91@gmail.com>
2023-02-08 14:17:25 +0300

    main_block in run.py added

--- run.py
+++ run.py
@@ -1,5 +1,18 @@
-from coding_paper.coding_paper import *
+from coding_paper.coding_paper import CodingPaper
+from input_window.input_window import Window, Stage
+from tkinter import *
+import os
+
 if __name__ == '__main__':
-    root = Tk()
-    c = CodingPaper()
+    root = Window()
     root.mainloop()
+    print(Stage.date)
+    config: dict = Stage.date
+    config["amount"] = 20
+    config["file_read"] = False
+    fp = os.path.join(os.getcwd(), "garbage", f"{config.get('title')}.json")
+    config["file_path"] = fp
+    root_2 = Tk()
+    cp = CodingPaper(root_2, cp_config=config)
+    cp.pack()
+    root_2.mainloop()
