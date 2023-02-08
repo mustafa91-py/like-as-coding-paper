69a13647181aab2bf3c932beb3695252759cb456
Mustafa UYAR <Mustafauyar91@gmail.com>
2023-02-08 13:49:14 +0300

    input_window added master without conflict

--- input_window/input_window.py
+++ input_window/input_window.py
@@ -2,6 +2,10 @@ from tkinter import *
 from tkinter import ttk
 
 
+class Stage:
+    date: dict = None
+
+
 class InputFrame(Frame):
     def __init__(self, parent, *args, **kwargs):
         super(InputFrame, self).__init__(parent, *args, **kwargs)
@@ -15,19 +19,43 @@ class InputFrame(Frame):
         self.subject_cbox = ttk.Combobox(self)
         self.subject_cbox.grid(row=1, column=1)
 
-        self.name_ = Label(self, text="name")
-        self.name_.grid(row=2, column=0)
-        self.name_entry = Entry(self)
-        self.name_entry.grid(row=2, column=1)
+        self.title = Label(self, text="name")
+        self.title.grid(row=2, column=0)
+        self.title_entry_var = StringVar()
+        self.title_entry = Entry(self, textvariable=self.title_entry_var)
+        self.title_entry.grid(row=2, column=1)
 
 
 class Window(Tk):
     def __init__(self):
         super(Window, self).__init__()
+        self.protocol("WM_DELETE_WINDOW", exit)
         self.top_frame = InputFrame(self)
         self.top_frame.pack()
+        self.save = Button(self, text="save", command=self.get)
+        self.save.pack()
+
+    def get(self):
+        out = dict(lesson=self.top_frame.lesson_cbox.get(),
+                   subject=self.top_frame.subject_cbox.get(),
+                   title=self.top_frame.title_entry_var.get())
+        Stage.date = out
+        self.destroy()
 
 
 if __name__ == '__main__':
+    from coding_paper.coding_paper import CodingPaper
+    import os
+
     root = Window()
     root.mainloop()
+    print(Stage.date)
+    config: dict = Stage.date
+    config["amount"] = 20
+    config["file_read"] = False
+    fp = os.path.join(os.getcwd(), "../garbage", f"{config.get('title')}.json")
+    config["file_path"] = fp
+    root_2 = Tk()
+    cp = CodingPaper(root_2, cp_config=config)
+    cp.pack()
+    root_2.mainloop()
