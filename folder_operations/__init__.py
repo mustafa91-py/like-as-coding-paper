eee32f5b8dd1493644224bc2bf907a035b0ba62f
Mustafa UYAR <Mustafauyar91@gmail.com>
2023-02-11 19:00:38 +0300

    added __init__ sshot file_path

--- folder_operations/__init__.py
+++ folder_operations/__init__.py
@@ -2,6 +2,8 @@ import os
 
 MAIN_FOLDER_NAME = M_F_N = "CodingPaper"
 FOLDER_PATH = F_P = os.path.join(os.path.expanduser("~"), M_F_N)
-if not os.path.exists(F_P):
-    os.mkdir(F_P)
-    os.startfile(F_P)
+SCREEN_SHOT_PATH=SS_SHOT = os.path.join(F_P,"SS_SHOT")
+for __ in [F_P,SS_SHOT]:
+    if not os.path.exists(__):
+        os.mkdir(__)
+        os.startfile(__)
