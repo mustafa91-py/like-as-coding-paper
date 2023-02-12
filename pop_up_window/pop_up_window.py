d0871c170319533cf81afd9aef25fcbee1151c28
Mustafa UYAR <Mustafauyar91@gmail.com>
2023-02-12 14:52:36 +0300

    pop_up_window.py

---
+++ pop_up_window/pop_up_window.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+
+class TopFrame(Frame):
+    def __init__(self, parent, *args, **kwargs):
+        super(TopFrame, self).__init__(parent, *args, **kwargs)
+
+
+class PopUpWindow(Toplevel):
+    def __init__(self):
+        super(PopUpWindow, self).__init__()
