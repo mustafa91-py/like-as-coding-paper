0b7e6a0d6c7be78585d5d535cfbd89bb64fc5d3e
Mustafa UYAR <Mustafauyar91@gmail.com>
2023-02-19 23:43:02 +0300

    revamp run ...

--- run.py
+++ run.py
@@ -1,19 +1,10 @@
 from coding_paper.coding_paper import CodingPaper,asdict
-from input_window.input_window import Window, Stage
+from input_window.input_window import Window, Stage,InputFrame
 from tkinter import *
 import folder_operations as fop
 import os
 
 if __name__ == '__main__':
-    root = Window()
-    root.mainloop()
-    print(Stage.config)
-    config: dict = Stage.config
-    config["amount"] = 5
-    file_name = f"{config.get('lesson')}_{config.get('subject')}_{config.get('title')}.json"
-    fp = os.path.join(fop.FOLDER_PATH,file_name)
-    config["file_path"] = fp
-    print(config)
 
     def save_exit():
         cp.save_dict.space = asdict(cp.container)
@@ -22,6 +13,8 @@ if __name__ == '__main__':
 
 
     root_2 = Tk()
+    ipf = InputFrame(root_2)
+    ipf.pack()
     root_2.protocol("WM_DELETE_WINDOW", save_exit)
     cp = CodingPaper(root_2, cp_config=config)
     cp.pack()
