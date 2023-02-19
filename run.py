d14d4e569f1220a8a045ed81f220c09ee79d6140
Mustafa UYAR <Mustafauyar91@gmail.com>
2023-02-19 23:57:59 +0300

    revamp run .1

--- run.py
+++ run.py
@@ -1,21 +1,32 @@
-from coding_paper.coding_paper import CodingPaper,asdict
-from input_window.input_window import Window, Stage,InputFrame
+import folder_operations
+from coding_paper.coding_paper import CodingPaper, asdict
+from input_window.input_frame import InputFrame
 from tkinter import *
 import folder_operations as fop
 import os
 
 if __name__ == '__main__':
-
-    def save_exit():
-        cp.save_dict.space = asdict(cp.container)
-        cp.save_dict.save()
-        root_2.destroy()
-
-
     root_2 = Tk()
-    ipf = InputFrame(root_2)
-    ipf.pack()
-    root_2.protocol("WM_DELETE_WINDOW", save_exit)
-    cp = CodingPaper(root_2, cp_config=config)
-    cp.pack()
+
+
+    def okay():
+        input_f.take_kwargs()
+        kwargs = input_f.out_kw
+        file_path = os.path.join(folder_operations.F_P, kwargs.get("title")+".json")
+        kwargs.update(dict(file_path=file_path))
+
+        def save_exit():
+            cp.save_dict.space = asdict(cp.container)
+            cp.save_dict.save()
+            root_2.destroy()
+
+        root_2.protocol("WM_DELETE_WINDOW", save_exit)
+        cp = CodingPaper(root_2, cp_config=kwargs)
+        cp.pack()
+
+
+    input_f = InputFrame(root_2)
+    input_f.pack()
+    input_f.create_paper.configure(command=okay)
+
     root_2.mainloop()
