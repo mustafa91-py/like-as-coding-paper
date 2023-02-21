51d1f1d430782f7da68545e1dd2e605babe8bd29
Mustafa UYAR <Mustafauyar91@gmail.com>
2023-02-21 20:03:04 +0300

    open_test.py added try code

---
+++ test_open.py
@@ -0,0 +1,24 @@
+import os.path
+
+import folder_operations
+from coding_paper.coding_paper import CodingPaperOpen
+from tkinter import *
+
+if __name__ == '__main__':
+    fff = os.path.join(folder_operations.F_P, "s.json")
+    root = Tk()
+    print(fff)
+    cp = CodingPaperOpen(root, file_path=fff)
+    cp.stain()
+    print(cp.container.get_data())
+    cp.pack(fill="both", expand=1)
+
+
+    def save_exit():
+        cp.save_dict.save()
+        root.destroy()
+    root.protocol("WM_DELETE_WINDOW", save_exit)
+
+
+    # print(cp.container)
+    root.mainloop()
