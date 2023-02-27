b67398f8eb68ddbf0ecb0cf66db472dc8e984cce
Mustafa UYAR <Mustafauyar91@gmail.com>
2023-02-27 23:08:42 +0300

    testing with openfilepath

--- test_open.py
+++ test_open.py
@@ -3,13 +3,18 @@ import os.path
 import folder_operations
 from coding_paper.coding_paper import CodingPaperOpen
 from tkinter import *
+from tkinter.filedialog import askopenfilename
 
 if __name__ == '__main__':
-    fff = os.path.join(folder_operations.F_P, "w_w_w.json")
+    try:
+        file_path = askopenfilename(initialdir=os.path.join(folder_operations.F_P))
+        print(f"{file_path=}")
+    except FileNotFoundError:
+        file_path = os.path.join(folder_operations.F_P, "w_w_w.json")
     root = Tk()
-    print(fff)
-    cp = CodingPaperOpen(root, file_path=fff)
+    cp = CodingPaperOpen(root, file_path=file_path)
     cp.stain()
+    root.wm_attributes("-topmost",1)
     print(cp.container.get_data())
     cp.pack(fill="both", expand=1)
 
